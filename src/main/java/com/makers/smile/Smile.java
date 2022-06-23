ee44bdf369731c474696e62718fb7c5674c3e68c
Jason Humbert <jasonhumbert@outlook.com>
2022-06-23 18:05:06 +0100

    Add Smile class and repository

--- src/main/java/com/makers/smile/Smile.java
+++ src/main/java/com/makers/smile/Smile.java
@@ -1,5 +1,46 @@
 package com.makers.smile;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Smile {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private Boolean isCrying = null;
+
+  private Boolean isLaughing = null;
+
+  public Smile() {}
+
+  public Smile(Boolean isCrying, Boolean isLaughing) {
+    this.isCrying = isCrying;
+    this.isLaughing = isLaughing;
+  }
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public Boolean getIsCrying() {
+    return this.isCrying;
+  }
+
+  public void setIsCrying(Boolean isCrying) {
+    this.isCrying = isCrying;
+  }
+
+  public Boolean getIsLaughing() {
+    return this.isLaughing;
+  }
+
+  public void setIsLaughing(Boolean isLaughing) {
+    this.isLaughing = isLaughing;
+  }
   
 }
--- src/test/java/com/makers/smile/SmileTest.java
+++ src/test/java/com/makers/smile/SmileTest.java
@@ -1,11 +1,42 @@
 package com.makers.smile;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class SmileTest {
     
     @Test
     void testConstruct() {
+        Smile smile = new Smile();
+
+        assertEquals(null, smile.getIsCrying());
+        assertEquals(null, smile.getIsLaughing());
+
+        smile = new Smile(true, false);
+        assertEquals(true, smile.getIsCrying());
+
+        smile = new Smile(false, true);
+        assertEquals(true, smile.getIsLaughing());
+    }
+
+    @Test
+    void testSetIsCrying() {
+        Smile smile = new Smile();
+        smile.setIsCrying(true);
+        assertEquals(true, smile.getIsCrying());
+
+        smile.setIsCrying(false);
+        assertEquals(false, smile.getIsCrying());
+    }
+
+    @Test
+    void testSetIsLaughing() {
+        Smile smile = new Smile();
+        smile.setIsLaughing(true);
+        assertEquals(true, smile.getIsLaughing());
         
+        smile.setIsLaughing(false);
+        assertEquals(false, smile.getIsLaughing());
     }
 }
